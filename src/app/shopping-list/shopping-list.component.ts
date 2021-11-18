8df3508737b49169e9df95943f346fcf5b695140
unknown <micheleghis@gmail.com>
2021-11-18 11:15:09 +0100

    Recipe and ingredient component layed out

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IngredientModel } from '../models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: IngredientModel[] = [
+    new IngredientModel("Pasta", 500),
+    new IngredientModel("Pomodoro", 2)
+  ]
 
   constructor() { }
 
