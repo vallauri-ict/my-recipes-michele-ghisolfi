e03d7058669b7817a471572cab0963b377e4f8a7
unknown <micheleghis@gmail.com>
2022-01-20 10:13:41 +0100

    highlight component update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
+    HighlightDirective,
   ],
   imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
